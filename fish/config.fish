3352de9d9de7b75c72077d0366df760766745f1e
tanmen <yt.prog@gmail.com>
2019-02-27 15:05:06 +0900

    Fisherman to fisher and bug fix

--- fish/config.fish
+++ fish/config.fish
@@ -11,16 +11,22 @@ set -g fish_user_paths $HOME/.anyenv/bin $fish_user_paths
 status --is-interactive; and source (anyenv init - fish|psub)
 
 # rubyのalias
-set GEM_ROOT (ruby -e 'print Gem.user_dir')
+set -gx GEM_ROOT (ruby -e 'print Gem.user_dir')
 
 # java
-set -x JAVA_HOME (/usr/libexec/java_home)
+set -gx JAVA_HOME (/usr/libexec/java_home)
 set -g fish_user_paths (string join / (/usr/libexec/java_home -v 1.8) bin) $fish_user_paths
 
 # openssl
 set -g fish_user_paths "/usr/local/opt/openssl/bin" $fish_user_paths
-set -gx LDFLAGS "-L/usr/local/opt/openssl/lib"
-set -gx CPPFLAGS "-I/usr/local/opt/openssl/include"
+set -gx LDFLAGS "-L/usr/local/opt/openssl/lib" $LDFLAGS
+set -gx CPPFLAGS "-I/usr/local/opt/openssl/include" $CPPFLAGS
+set -gx PKG_CONFIG_PATH "/usr/local/opt/openssl/lib/pkgconfig" $PKG_CONFIG_PATH
+
+# mysql
+set -g fish_user_paths "/usr/local/opt/mysql-client/bin" $fish_user_paths
+set -gx LDFLAGS "-L/usr/local/opt/mysql-client/lib" $LDFLAGS
+set -gx CPPFLAGS "-I/usr/local/opt/mysql-client/include" $CPPFLAGS
 
 # Tools
 set -g fish_user_paths $HOME/Tools/git-utils $fish_user_paths
--- fish/init.fish
+++ fish/init.fish
@@ -1,22 +1,18 @@
 #!/usr/local/bin/fish
 
 # fishのpackage managerをインストール
-curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisherman
+curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
 
 # config をセット
 ln -s ~/.dotfiles/fish/config.fish ~/.config/fish
 
 # (https://github.com/fisherman/z)
-fisher z
+fisher add z
 # (https://github.com/fisherman/fzf)
-fisher fzf
+fisher add fzf
 # fishでもbashがうごくようにする(https://github.com/edc/bass)
-fisher edc/bass
+fisher add edc/bass
 # (https://github.com/omf/theme-bobthefish)
-fisher omf/theme-bobthefish
-# nodeの補完が効く(https://github.com/oh-my-fish/plugin-node)
-# fisher oh-my-fish/plugin-node
-# gemの補完がきく(https://github.com/oh-my-fish/plugin-gem)
-fisher oh-my-fish/plugin-gem
+fisher add omf/theme-bobthefish
 # nestしたssh_configを補完できるように
-fisher masa0x80/complete_ssh_host.fish
+fisher add masa0x80/complete_ssh_host.fish
